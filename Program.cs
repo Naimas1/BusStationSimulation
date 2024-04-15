565de9fceb3613f42a103e18468fb984400cd42f
Denis Riabchuk <Naimas@DESKTOP-68Q4MHG>
2024-04-15 12:03:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BusStationSimulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
